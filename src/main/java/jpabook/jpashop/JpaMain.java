f5f11b9394b2fde1d0d210158a8a1a66ab3ee56f
nyeroni <nir2y@naver.com>
2023-10-13 00:20:19 +0900

    Update: 상품 예시

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 
 public class JpaMain {
 
@@ -14,6 +16,16 @@ public class JpaMain {
         tx.begin();
 
         try{
+
+            Book book = new Book();
+            book.setAuthor("yerin");
+            book.setIsbn("1123");
+            book.setName("babo");
+            book.setPrice(12212);
+            book.setStockQuantity(32);
+
+            em.persist(book);
+
             tx.commit();
 
         }catch (Exception e){
